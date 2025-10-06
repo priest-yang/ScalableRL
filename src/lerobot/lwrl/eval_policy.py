d5d70562636eb42301973bfa8bcfeb71161248cd
Shaoze <shaozey@umich.edu>
2025-10-06 01:27:29 -0400

    fixbug in eval policy, load policy object from pkl method works

--- src/lerobot/lwrl/eval_policy.py
+++ src/lerobot/lwrl/eval_policy.py
@@ -54,7 +54,7 @@ def parse_arguments():
 
     # model configuration
     parser.add_argument("--model_path", type=str, 
-                       default="/home/johndoe/Documents/lerobot-hilserl/outputs/train/2025-10-05/00-57-03_lwlab_lerobot_pickup_100env_nsteps3/checkpoints/0090000/pretrained_model",
+                       default="/home/johndoe/Documents/lerobot-hilserl/outputs/train/2025-10-05/00-57-03_lwlab_lerobot_pickup_100env_nsteps3/checkpoints/0110000/pretrained_model",
                        help="Model path")
 
     # hilserl args (for obs processing)
@@ -83,7 +83,9 @@ def eval_policy(env, env_processor, action_processor, policy, n_steps, cfg):
         observation = {
             k: v for k, v in transition[TransitionKey.OBSERVATION].items() if k in cfg.policy.input_features
         }
+
         action = policy.select_action(batch=observation)
+
         new_transition = step_lwlab_env_and_process_transition(
             env=env,
             transition=transition,
@@ -91,28 +93,61 @@ def eval_policy(env, env_processor, action_processor, policy, n_steps, cfg):
             env_processor=env_processor,
             action_processor=action_processor,
         )
+
         done = new_transition.get(TransitionKey.DONE, torch.tensor(False))
         truncated = new_transition.get(TransitionKey.TRUNCATED, torch.tensor(False))
         info = new_transition.get(TransitionKey.INFO, {})
+        reward = new_transition.get(TransitionKey.REWARD, torch.tensor(0.0, device=env.device, dtype=torch.float32))
 
         if info == {}:
             print("Warning: info is empty")
+
         num_success = info.get('is_success', torch.zeros_like(done, device=env.device, dtype=torch.bool)).sum().item()
         num_total = torch.logical_or(done, truncated).sum().item()
-
         sum_success += num_success
         sum_total += num_total
 
-        transition = new_transition
+        if torch.any(done) or torch.any(truncated):
+            new_transition_with_reset = new_transition
+            # re-write done and truncated
+            new_transition_with_reset[TransitionKey.DONE] = torch.zeros_like(done, device=env.device, dtype=torch.bool)
+            new_transition_with_reset[TransitionKey.TRUNCATED] = torch.zeros_like(truncated, device=env.device, dtype=torch.bool)
+            new_transition_with_reset[TransitionKey.REWARD] = torch.zeros_like(reward, device=env.device, dtype=torch.float32)
+            new_transition_with_reset[TransitionKey.INFO] = {}
+            #! original code will reset processor here, but skip here
+            # TODO: need to implement reset processor per env index
+            # env_processor.reset()
+            # action_processor.reset()
+            
+            # recreate real transition and overwrite next observation (pass processer)
+            next_observation_raw = info['final_obs']['policy'] # replace with last obs before reset
+            new_transition_raw = create_transition(
+                observation=next_observation_raw, info=info,
+                done=torch.zeros_like(done, device=env.device, dtype=torch.bool),
+                truncated=torch.zeros_like(truncated, device=env.device, dtype=torch.bool),
+                reward=torch.zeros_like(reward, device=env.device, dtype=torch.float32),
+            )
+            # Extract values from processed transition
+            new_transition = env_processor(new_transition_raw)
+            # make sure those will not be used!! (only create to use processer)
+            del new_transition, new_transition_raw
+
+            info.pop('final_obs') # remove final_obs from info to save space
+            
+        if torch.any(done) or torch.any(truncated):
+            transition = new_transition_with_reset
+        else:
+            transition = new_transition
 
     # print success rate
+    print(f"Total episodes: {sum_total}")
+    print(f"Success episodes: {sum_success}")
     print(f"Success rate: {sum_success / sum_total}")
-    print(f"Total steps: {sum_total}")
-    print(f"Success steps: {sum_success}")
 
         
 @parser.wrap()
 def main(cfg: TrainRLServerPipelineConfig):
+    cfg.validate()
     args = parse_arguments()
 
     torch.backends.cudnn.benchmark = True
@@ -129,6 +164,10 @@ def main(cfg: TrainRLServerPipelineConfig):
     )
     print("Loading policy...")
     policy.from_pretrained(args.model_path, local_files_only=True)
+    
+    import pickle as pkl
+    policy = pkl.load(open("/home/johndoe/Documents/lerobot-hilserl/outputs/train/2025-10-05/18-56-45_lwlab_lerobot_pickup_100env_nsteps3_debug_resume/policy_24000.pkl", "rb"))
+
     policy.eval()
 
     print("Evaluating policy...")
